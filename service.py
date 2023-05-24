dba1ae5e4dee5f0f9e6e61bc8e1969de99b87e08
samuelchovanec <samuel.chovanec@gmail.com>
2023-05-24 16:44:58 +0200

    Update service.py

--- service.py
+++ service.py
@@ -6,9 +6,9 @@ relay_ch = 26
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(relay_ch, GPIO.OUT)
-GPIO.output(relay_ch, GPIO.LOW)
-time.sleep(1)
-GPIO.output(relay_ch, GPIO.HIGH)
-GPIO.cleanup()
+while true: 
+  GPIO.setup(relay_ch, GPIO.OUT)
+  GPIO.output(relay_ch, GPIO.LOW)
+  time.sleep(1)
+  GPIO.output(relay_ch, GPIO.HIGH)
+  GPIO.cleanup()
